1872394e339a30cc09de7bfeee8fd401c490583b
gvns0x <gvns0x@gmail.com>
2025-09-09 17:11:30 +0530

    blob component

---
+++ src/components/chat/LoadingBlobs/LoadingBlobs.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useRef } from 'react';
+import './LoadingBlobs.css'
+
+const LoadingBlobs = () => {
+    const blobRefs = useRef([]);
+
+    useEffect(() => {
+        const blobs = blobRefs.current;
+
+        blobs.forEach((blob, i) => {
+            if (blob && window.gsap) {
+                window.gsap.to(blob, {
+                    x: () => 70,
+                    y: () => 70 * Math.random() / 10,
+                    duration: 2,
+                    repeat: -1,
+                    yoyo: true,
+                    ease: "sine.inOut",
+                    delay: 0
+                });
+            }
+        });
+    }, []);
+
+    return (
+        <div className='LoadingBlobs'>
+            <div ref={el => blobRefs.current[0] = el} className="blob"></div>
+            <div ref={el => blobRefs.current[1] = el} className="blob"></div>
+            <div ref={el => blobRefs.current[2] = el} className="blob"></div>
+        </div>
+    );
+};
+
+export default LoadingBlobs;
